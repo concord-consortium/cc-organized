f876e688c05c2fe0e9dc6fa70b72ae3b9b34f0e2
Scott Cytacki <scytacki@concord.org>
2011-10-06 22:08:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcOrganized::Application.config.secret_token = '8fd982882cc4967b03a6222655cad8b785be0d165aa28f30b71c0c890ebbb9e9bc50abb462d1002a673183dd0072f7c6712cbf05a286f69f4ca2bd6730a28fec'
